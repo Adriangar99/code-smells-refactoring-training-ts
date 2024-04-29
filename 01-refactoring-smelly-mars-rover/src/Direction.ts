d92bcc441b7ec829a906fe80d3c9ab7f7404bf32
Adrián García <adriangargar99@gmail.com>
2024-04-29 20:46:50 +0200

    extract methods

--- 01-refactoring-smelly-mars-rover/src/Direction.ts
+++ 01-refactoring-smelly-mars-rover/src/Direction.ts
@@ -4,3 +4,39 @@ export enum Direction {
   W = "W",
   E = "E",
 }
+
+export function isFacingNorth(direction: Direction) {
+  return direction === Direction.N;
+}
+
+export function isFacingSouth(direction: Direction) {
+  return direction === Direction.S;
+}
+
+export function isFacingWest(direction: Direction) {
+  return direction === Direction.W;
+}
+
+export function rotateLeft(direction: Direction) {
+  if (isFacingNorth(direction)) {
+    return Direction.W;
+  } else if (isFacingSouth(direction)) {
+    return Direction.E;
+  } else if (isFacingWest(direction)) {
+    return Direction.S;
+  } else {
+    return Direction.N;
+  }
+}
+
+export function rotateRight(direction: Direction) {
+  if (isFacingNorth(direction)) {
+    return Direction.E;
+  } else if (isFacingSouth(direction)) {
+    return Direction.W;
+  } else if (isFacingWest(direction)) {
+    return Direction.N;
+  } else {
+    return Direction.S;
+  }
+}
--- 01-refactoring-smelly-mars-rover/src/Rover.ts
+++ 01-refactoring-smelly-mars-rover/src/Rover.ts
@@ -1,20 +1,21 @@
-import { Direction } from "./Direction";
+import {
+  Direction,
+  isFacingNorth,
+  isFacingSouth,
+  isFacingWest,
+  rotateLeft,
+  rotateRight,
+} from "./Direction";
 
 export class Rover {
-  private direction: string;
   private y: number;
   private x: number;
-  private direction2: Direction;
+  private direction: Direction;
 
-  constructor(x: number, y: number, direction: string) {
+  constructor(x: number, y: number, direction: Direction) {
     this.x = x;
     this.y = y;
-    this.setDirection(direction);
-  }
-
-  private setDirection(direction: string) {
     this.direction = direction;
-    this.direction2 = direction as any as Direction;
   }
 
   public receive(commandsSequence: string) {
@@ -22,9 +23,9 @@ export class Rover {
       const command = commandsSequence.substring(i, i + 1);
 
       if (command === "l") {
-        this.rotateLeft();
+        this.direction = rotateLeft(this.direction);
       } else if (command === "r") {
-        this.rotateRight();
+        this.direction = rotateRight(this.direction);
       } else {
         // Displace Rover
         let displacement1 = -1;
@@ -34,52 +35,16 @@ export class Rover {
         }
         let displacement = displacement1;
 
-        if (this.isFacingNorth()) {
+        if (isFacingNorth(this.direction)) {
           this.y += displacement;
-        } else if (this.isFacingSouth()) {
+        } else if (isFacingSouth(this.direction)) {
           this.y -= displacement;
-        } else if (this.isFacingWest(this.direction2)) {
+        } else if (isFacingWest(this.direction)) {
           this.x -= displacement;
         } else {
           this.x += displacement;
         }
       }
-    }
-  }
-
-  private isFacingWest(direction: Direction) {
-    return direction === Direction.W;
-  }
-
-  private isFacingSouth() {
-    return this.direction2 === Direction.S;
-  }
-
-  private isFacingNorth() {
-    return this.direction2 === Direction.N;
-  }
-
-  private rotateLeft() {
-    if (this.isFacingNorth()) {
-      this.setDirection("W");
-    } else if (this.isFacingSouth()) {
-      this.setDirection("E");
-    } else if (this.isFacingWest(this.direction2)) {
-      this.setDirection("S");
-    } else {
-      this.setDirection("N");
-    }
-  }
-
-  private rotateRight() {
-    if (this.isFacingNorth()) {
-      this.setDirection("E");
-    } else if (this.isFacingSouth()) {
-      this.setDirection("W");
-    } else if (this.isFacingWest(this.direction2)) {
-      this.setDirection("N");
-    } else {
-      this.setDirection("S");
     }
   }
 }
